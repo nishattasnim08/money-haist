28747c20f12f1956e060f069460238cb4754f042
nishattasnim08 <nishattasnim24258@gmail.com>
2022-02-17 22:57:11 +0600

    converted float from string

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
+// getting all input value
+function getInput(inputFieldId) {
+    const inputValueText = document.getElementById(inputFieldId);
+    if (isNaN(inputValueText.value)) {
+        alert('You must enter a number');
+    }
+
+    else {
+        inputValue = parseFloat(inputValueText);
+    }
+    if (inputValue >= 0) {
+
+        return inputValue;
+
+    }
+    else {
+        alert('You must enter a posittive number')
+    }
+}
+
+// balance calculation
+
+function balenceAmountCalculator(income,food,rent,cloths){
+    const balence = income - expence ;
+
+    if(balence < 0){
+        alert("vai apni khoroc bashi kore felcen, tai income baran")
+    }
+    else{
+        document.getElementById('expence').innerText = expence;
+        document.getElementById('balance').innerText = balence;
+
+    }
+}
+
 // handle calculate button event
 
 document.getElementById('calculateButton').addEventListener('click', function () {
-    const incomeInput = document.getElementById('inputIncome');
-    const incomeAmountText = incomeInput.value;
-    const incomeAmount = parseFloat(incomeAmountText);
-    // current income amount
-    const totalBalance = document.getElementById('balanceAmount');
-    const totalBalanceText = totalBalance.innerText;
-    const previousTotalBalance = parseFloat(totalBalanceText);
+    const incomeAmount = getInput("inputIncome");
+    const foodExpenseAmount = getInput("inputFood");
+    const rentExpenseAmount = getInput("inputRent");
+    const clothEpenseAmount = getInput("inputCloths");
 
-    // update balance
-    totalBalance.innerText = previousTotalBalance + incomeAmount;
-
-    // clear income input field
-    incomeInput.value = '';
-
-// current expense amount
-const 
+    balenceAmountCalculator(incomeAmount, foodExpenseAmount, rentExpenseAmount, clothEpenseAmount)
 })
